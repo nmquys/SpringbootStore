bf76292823cac6e6fd21be3477bd12d4f8484ea7
Minh Quý <mquy2810@gmail.com>
2025-10-14 13:10:32 +0700

    init

---
+++ src/main/java/com/nmquys/springbootstore/SpringbootStoreApplication.java
@@ -0,0 +1,15 @@
+package com.nmquys.springbootstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStoreApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringbootStoreApplication.class, args);
+    }
+
+}
