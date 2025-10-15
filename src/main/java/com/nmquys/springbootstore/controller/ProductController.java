38ffd4b6b286a724a712b98455e4f8b3f127acd0
Minh Quý <mquy2810@gmail.com>
2025-10-15 17:19:13 +0700

    adjust format layer

--- src/main/java/com/nmquys/springbootstore/controller/ProductController.java
+++ src/main/java/com/nmquys/springbootstore/controller/ProductController.java
@@ -1,16 +1,25 @@
 package com.nmquys.springbootstore.controller;
 
+import com.nmquys.springbootstore.dto.ProductDto;
+import com.nmquys.springbootstore.service.IProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/products")
+@RequiredArgsConstructor
 public class ProductController
 {
+    private final IProductService iProductService;
+
     @GetMapping
-    public String getProducts()
-    {
-        return "Here are ur products";
+    public List<ProductDto> getProducts()
+    { // DTO Pattern
+        List<ProductDto> productList = iProductService.getProducts();
+        return productList;
     }
 }
