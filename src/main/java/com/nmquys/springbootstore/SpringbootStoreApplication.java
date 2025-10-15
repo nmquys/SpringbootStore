3d22e2e04eb611f512e5b97104023533c5d7b257
Minh Quý <mquy2810@gmail.com>
2025-10-15 17:19:13 +0700

    updated sample sql

--- src/main/java/com/nmquys/springbootstore/SpringbootStoreApplication.java
+++ src/main/java/com/nmquys/springbootstore/SpringbootStoreApplication.java
@@ -2,6 +2,8 @@ package com.nmquys.springbootstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringbootStoreApplication
---
+++ src/main/java/com/nmquys/springbootstore/entity/Product.java
@@ -0,0 +1,49 @@
+package com.nmquys.springbootstore.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product
+{
+    //this make product_id as a primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "name", nullable = false)
+    private String productName;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "popualarity", nullable = false)
+    private Integer populartity;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
